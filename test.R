3055ea9db0170db52a80a6c59b0e80a53519c8dc
luthec <tiancliu@gmail.com>
2023-07-05 19:45:15 +0800

    1

--- test.R
+++ test.R
@@ -5,6 +5,12 @@ library(ggpubr)
 library(ComplexHeatmap)
 library(RColorBrewer)
 library(gridGraphics)
+library(tidyverse)
+library(lmerTest)
+library(lme4)
+library(broom)
+library(broom.mixed)
+library(gtools)
 #library(Cairo)
 
 empyrosis_data2 <- read_excel("empyrosis_data2.xlsx", 
@@ -86,6 +92,58 @@ tb2 = print(
   nonnormal = c("TBSA","烧伤指数"),
   showAllLevels = TRUE)   
 
+
+
+biomarker="Key_Value"
+
+dt_select = empyrosis_data2 %>% 
+            select(c("主要结局：耐受0，不耐受1","年龄","BMI","TBSA","烧伤指数","III度",matches("@1d"),"呕吐（无0有1）","呕吐（无呕吐0，休克期1，非休克期2，两者都有3）","脓毒症（无0有1）","鼻饲（有1无0）","腹泻","CRRT","@90天死亡（成活0死亡1）")) %>% 
+            mutate_at(vars(!c("BMI","TBSA","烧伤指数","III度",matches("@1d"))), as.factor) %>%
+            mutate_at(vars("年龄"), as.numeric)
+
+
+colnames(dt_select)[c(1,19:25)]=c("耐受","呕吐","呕吐时期","脓毒症","鼻饲","腹泻","CRRT","死亡")
+
+dt_na_removed <- na.omit(dt_select)
+
+#fit.glm = glm(耐受 ~ 年龄+BMI+TBSA+烧伤指数+III度+呕吐+呕吐时期+脓毒症+鼻饲+腹泻+CRRT+死亡+`@1d_TBIL`+`@1d_DBIL`+`@1d_BUN`+`@1d_LAC`+`@1d_CRE`+`@1d_hct`+`@1d_ALB`+`@1dHB`+`@1d_淋巴细胞`+`@1d_plt`+`@1d_PA`+`@1d_TP`, family = binomial,data=dt_na_removed) %>%
+#    tidy(conf.int = TRUE) %>% 
+#    select(c("term","estimate","std.error","conf.low","conf.high","p.value")) %>% 
+#    mutate(signif = stars.pval(p.value))
+
+fit.glm = glm(耐受 ~ 年龄+BMI+TBSA+烧伤指数+III度+脓毒症+`@1d_TBIL`+`@1d_DBIL`+`@1d_BUN`+`@1d_LAC`+`@1d_CRE`+`@1d_hct`+`@1d_ALB`+`@1dHB`+`@1d_淋巴细胞`+`@1d_plt`+`@1d_PA`+`@1d_TP`, family = binomial,data=dt_na_removed) %>%
+    tidy(conf.int = TRUE) %>% 
+    select(c("term","estimate","std.error","conf.low","conf.high","p.value")) %>% 
+    mutate(signif = stars.pval(p.value))   
+
+    
+main.title <- paste0(biomarker,"_GLM")
+subtitle <- paste0("Generalized Linear Model") %>%
+  strwrap(width = 80) %>%
+  paste(collapse = "\n")
+t.glm <- ggtexttable(fit.glm, theme = ttheme("light")) %>%
+  tab_add_title(text = subtitle, face = "plain", size = 10) %>%
+  tab_add_title(text = main.title, face = "bold", padding = unit(0.1, "line")) %>%
+  tab_add_footnote(text = "* means significant levels", size = 10, face = "italic")
+
+
+fit.mem <- glmer(耐受 ~ 年龄+BMI+TBSA+烧伤指数+III度+(1 | `脓毒症`)+`@1d_TBIL`+`@1d_DBIL`+`@1d_BUN`+`@1d_LAC`+`@1d_CRE`+`@1d_hct`+`@1d_ALB`+`@1dHB`+`@1d_淋巴细胞`+`@1d_plt`+`@1d_PA`+`@1d_TP`, data = dt_na_removed, family = binomial, control = glmerControl(optimizer = "bobyqa")) %>%
+    tidy(conf.int = TRUE) %>% 
+    select(c("term","estimate","std.error","conf.low","conf.high","p.value")) %>% 
+    mutate(signif = stars.pval(p.value))
+
+main.title <- paste0(biomarker,"_GMM")
+subtitle <- paste0("Generalized linear mixed model") %>%
+  strwrap(width = 80) %>%
+  paste(collapse = "\n")
+t.mem <- ggtexttable(fit.mem, theme = ttheme("light")) %>%
+  tab_add_title(text = subtitle, face = "plain", size = 10) %>%
+  tab_add_title(text = main.title, face = "bold", padding = unit(0.1, "line")) %>%
+  tab_add_footnote(text = "* means significant levels", size = 10, face = "italic")
+
+pcom = ggarrange(t.glm,t.mem, ncol = 2, nrow = 1,widths=c(1, 1))
+
+
 outpdf=paste("res","_profile_new.pdf",sep='')
 pdf(outpdf, width = 16, height = 10, family="GB1")
 
@@ -112,5 +170,7 @@ t1 <- ggtexttable(as.data.frame(tb1), theme = ttheme("light"))
 t2 <- ggtexttable(as.data.frame(tb2), theme = ttheme("light"))      
 tcom = ggarrange(t1,t2, ncol = 2, nrow = 1,widths=c(1, 1))
 print(tcom)
+
+print(pcom)
 
 dev.off()
