270b103d086d36747b7969fe0052ec1770b87fb7
luthec <tiancliu@gmail.com>
2023-10-29 20:39:10 +0800

    1

--- CCL_scRNA_test.R
+++ CCL_scRNA_test.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(Seurat)
+library(metap)
 
 folders=list.files('./')
 folders
@@ -16,31 +17,24 @@ for(i in 1:length(folders)){
 names(sceList)  = folders
 sum(sapply(sceList, function(x)ncol(x@assays$RNA@counts)))
 
-sceList <- lapply(X = sceList, FUN = function(x) {
-    x <- NormalizeData(x)
-    x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
-  })
+# Set up control object
+nc <- merge(x = sceList[[8]], y = sceList[[9]], add.cell.ids = names(sceList)[8:9] , project = "NC")
+nc$label <- "NC"
+nc <- NormalizeData(nc, verbose = FALSE)
+nc <- FindVariableFeatures(nc, selection.method = "vst", nfeatures = 2000)
+
+# Set up case object
+ccl <- merge(x = sceList[[1]], y = sceList[2:6], add.cell.ids = names(sceList)[1:6] , project = "CCL")
+ccl$label <- "CCL"
+ccl <- NormalizeData(ccl, verbose = FALSE)
+ccl <- FindVariableFeatures(ccl, selection.method = "vst", nfeatures = 2000)
 
 ###normalize, integration and filter
-normal.features <- SelectIntegrationFeatures(object.list = sceList[8:9])
-normal.anchors <- FindIntegrationAnchors(object.list = sceList[8:9], anchor.features = normal.features)
-normal.combined <- IntegrateData(anchorset = normal.anchors)
-DefaultAssay(normal.combined) <- "normal_integrated"
-
-ccl.features <- SelectIntegrationFeatures(object.list = sceList[1:6])
-ccl.anchors <- FindIntegrationAnchors(object.list = sceList[1:6], anchor.features = ccl.features)
-ccl.combined <- IntegrateData(anchorset = ccl.anchors)
-DefaultAssay(ccl.combined) <- "ccl_integrated"
-
+ccl.anchors <- FindIntegrationAnchors(object.list = list(nc, ccl), dims = 1:20)
+ccl.combined <- IntegrateData(anchorset = ccl.anchors, dims = 1:20)
+DefaultAssay(ccl.combined) <- "integrated"
 
 #######UMAP
-
-normal.combined <- ScaleData(normal.combined, verbose = FALSE)
-normal.combined <- RunPCA(normal.combined, npcs = 30, verbose = FALSE)
-normal.combined <- RunUMAP(normal.combined, reduction = "pca", dims = 1:30)
-normal.combined <- FindNeighbors(normal.combined, reduction = "pca", dims = 1:30)
-normal.combined <- FindClusters(normal.combined, resolution = 0.5)
-
 
 ccl.combined <- ScaleData(ccl.combined, verbose = FALSE)
 ccl.combined <- RunPCA(ccl.combined, npcs = 30, verbose = FALSE)
@@ -48,12 +42,28 @@ ccl.combined <- RunUMAP(ccl.combined, reduction = "pca", dims = 1:30)
 ccl.combined <- FindNeighbors(ccl.combined, reduction = "pca", dims = 1:30)
 ccl.combined <- FindClusters(ccl.combined, resolution = 0.5)
 
+p1 <- DimPlot(ccl.combined, reduction = "umap", group.by = "orig.ident")
+p2 <- DimPlot(ccl.combined, reduction = "umap", label = TRUE, repel = TRUE)
 
-p1 <- DimPlot(normal.combined, reduction = "umap", group.by = "orig.ident")
-p2 <- DimPlot(normal.combined, reduction = "umap", label = TRUE, repel = TRUE)
+p1 + p2 
+
+DimPlot(ccl.combined, reduction = "umap", split.by = "label")
+
+#### Identify conserved cell type markers
+
+DefaultAssay(ccl.combined) <- "RNA"
+con.markers <- FindConservedMarkers(ccl.combined, ident.1 = 0, grouping.var = "label", verbose = FALSE)
+head(con.markers)
+
+FeaturePlot(ccl.combined, features = c("CD3D", "SELL", "CREM", "CD8A", "GNLY", "CD79A", "FCGR3A", 
+    "CCL2", "PPBP"), min.cutoff = "q9")
 
 
-p3 <- DimPlot(ccl.combined, reduction = "umap", group.by = "orig.ident")
-p4 <- DimPlot(ccl.combined, reduction = "umap", label = TRUE, repel = TRUE)
+ccl.markers <- FindAllMarkers(ccl.combined, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+ccl.markers %>%
+    group_by(cluster) %>%
+    slice_max(n = 2, order_by = avg_log2FC)
 
-p1 + p2 + p3 + p4
+IL7R, CCR7
+
+VlnPlot(ccl.combined, features = c("IL7R", "CCR7"))
