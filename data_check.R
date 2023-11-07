93cd93ff26d92e7eb69d4f422ae55c323a890df6
luthec <tiancliu@gmail.com>
2023-11-07 20:01:02 +0800

    1

--- data_check.R
+++ data_check.R
@@ -5,8 +5,11 @@ library(readxl)
 
 dt = read_excel(dir()[2],sheet = 2,col_names = T)
 
+ins = read.csv(dir()[2], header = TRUE,skip=4)
 
+tg_ins = ins %>% as.tibble() %>% filter(检测名=="ThygIUO") %>% filter(grepl("^H",样本编号))  %>% select(样本编号,用户结果)
 
+用户结果
 table(dt$组别)
 
 
@@ -17,7 +20,7 @@ table(dtt$样本编号)
 
 table(dt$性别)
 
-sapply(dt$`年龄（岁）`, is.numeric)
+sapply(dt$`年龄`, is.numeric)
 
 dtt = dt %>% filter(!grepl("^\\d+", `NT-proBNP 检测结果（ng/L）`))
 table(dtt$`NT-proBNP 检测结果（ng/L）`)
