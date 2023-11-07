734a48ac715f8b77b5859a04ea5dfc3c722c0c61
luthec <tiancliu@gmail.com>
2023-11-07 18:17:23 +0800

    1

--- scRNA.R
+++ scRNA.R
@@ -7,6 +7,28 @@ library(ggpubr)
 rm(list = ls())
 gc()
 
+SCT_intergration <- function(assay_list,n_dim=20) {  
+features <- SelectIntegrationFeatures(object.list = assay_list, nfeatures = 3000)
+datasets <- PrepSCTIntegration(object.list = assay_list, anchor.features = features, verbose = TRUE)
+#datasets <- lapply(X = datasets, FUN = RunPCA, verbose = FALSE, features = features)
+anchors <- FindIntegrationAnchors(object.list = datasets, normalization.method = "SCT",anchor.features = features, verbose = TRUE, reference=1,reduction = "cca")
+objs <-IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE, k.weight = 46)
+
+objs <- RunPCA(objs, verbose = FALSE, approx = FALSE, npcs = 30)
+# objs <- RunUMAP(objs, reduction = "pca", dims = 1:30, umap.method = "umap-learn", metric = "correlation")
+# objs <- RunTSNE(objs, reduction = "pca",dims = 1:30)
+objs <- RunUMAP(objs, reduction = "pca", dims = 1:n_dim)
+objs <- FindNeighbors(objs, reduction = "pca",dims = 1:n_dim)
+FindClusters(objs, resolution = 0.5, algorithm = 2)
+}
+
+
+all_markers.to.plot <- c("CD4","CD8A","CD8B","CD3D", "CREM", "HSPH1", "SELL", "GIMAP5", "CACYBP", "GNLY", "NKG7", "CCL5", 
+     "MS4A1", "CD79A", "MIR155HG", "NME1", "FCGR3A", "VMO1", "CCL2", "S100A9", "HLA-DQA1", 
+    "GPR183", "PPBP", "GNG11", "HBA2", "HBB", "TSPAN13", "IL3RA", "IGJ")
+
+tcell_markers.to.plot <- c("CD4","CD8A", "CD8B",'CD40LG',"CCR7", "IL7R", "SELL", "DTHD1", "LYST", "LEF1", "CCR4", "GZMH")
+
 folders=list.files('./')
 folders
 
@@ -32,56 +54,95 @@ jsh <- sceList[[7]] %>% SCTransform(verbose = FALSE)
 jsh$label <- "JSH"
 
 sample_list=c(nc,ccl,jsh)
-features <- SelectIntegrationFeatures(object.list = sample_list, nfeatures = 2000)
-datasets <- PrepSCTIntegration(object.list = sample_list, anchor.features = features, verbose = TRUE)
-datasets <- lapply(X = datasets, FUN = RunPCA, verbose = FALSE, features = features)
-anchors <- FindIntegrationAnchors(object.list = datasets, normalization.method = "SCT",anchor.features = features, verbose = TRUE, reference=1,reduction = "cca")
-objs <- IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE)
+#features <- SelectIntegrationFeatures(object.list = sample_list, nfeatures = 2000)
+#datasets <- PrepSCTIntegration(object.list = sample_list, anchor.features = features, verbose = TRUE)
+#datasets <- lapply(X = datasets, FUN = RunPCA, verbose = FALSE, features = features)
+#anchors <- FindIntegrationAnchors(object.list = datasets, normalization.method = "SCT",anchor.features = features, verbose = TRUE, reference=1,reduction = "cca")
+#objs <- IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE)
 
-objs <- RunPCA(objs, verbose = FALSE, approx = FALSE, npcs = 30)
+#objs <- RunPCA(objs, verbose = FALSE, approx = FALSE, npcs = 30)
 # objs <- RunUMAP(objs, reduction = "pca", dims = 1:30, umap.method = "umap-learn", metric = "correlation")
 # objs <- RunTSNE(objs, reduction = "pca",dims = 1:30)
-objs <- RunUMAP(objs, reduction = "pca", dims = 1:30)
-objs <- FindNeighbors(objs, reduction = "pca",dims = 1:30)
-objs <- FindClusters(objs, resolution = 0.5, algorithm = 2)
+#objs <- RunUMAP(objs, reduction = "pca", dims = 1:30)
+#objs <- FindNeighbors(objs, reduction = "pca",dims = 1:30)
+objs <- SCT_intergration(sample_list,30)
 
 DefaultAssay(objs) <- 'SCT'
 
 p1 <- DimPlot(objs, reduction = "umap", group.by = "orig.ident")
 p2 <- DimPlot(objs, reduction = "umap", label = TRUE, repel = TRUE)
-
 p3 <- DimPlot(objs, reduction = "umap", split.by = "label")
-p4 <- FeaturePlot(objs, features = c("CD8A", "IL7R", "CCR7", "S100A4", "GNLY", "NKG7", "FCGR3A", "MS4A7", "CD14"), min.cutoff = "q9")
+# p4 <- FeaturePlot(objs, features = c("CD8A", "IL7R", "CCR7", "S100A4", "GNLY", "NKG7", "FCGR3A", "MS4A7", "CD14"), min.cutoff = "q9")
+
+#### Identify   cell type  
+
+library(celldex)
+library(SingleR)
+hpc <- celldex::HumanPrimaryCellAtlasData() 
+# hpc <- get(load("../single_ref/ref_Human_all.RData"))
+library(BiocParallel)
+pred.scRNA <- SingleR(test = objs@assays$integrated@data, 
+                      ref = hpc,
+                      labels = hpc$label.main, 
+                      clusters = objs@active.ident)
+
+p33 = plotScoreHeatmap(pred.scRNA, clusters=pred.scRNA@rownames, fontsize.row = 9,show_colnames = T)
+
+pred.scRNA %>% filter(labels=="T cell")
+
+# T-Cell Markers
+signature <- readxl::read_excel('C:\\Users\\tliu05\\Desktop\\2023projects\\01_DS\\jiangsu\\Cell_marker_Human.xlsx')
+sig_gene  <- signature %>% as.data.frame() %>% filter(`Tissue type`=="Peripheral blood" & `Cell type`== "Cancer cell") %>% mutate(V1=`Cell name`,V2=Symbol) %>% select(V1,V2)  %>% unique(.) 
+sig_gene %>% filter(V1=="CD8 T cell" | V1=="CD4 T cell" | V1=="T cell")
+sig_gene %>% filter(grepl("T cell",V1))
+
+p4 <- FeaturePlot(objs, features = c("CD8A","CD8B","CD4","CD3D","CD3E","CD3G"),  label = TRUE)
+plots0 <- VlnPlot(objs, features = c("CD4", "CD8A","CD8B"), split.by = "label", slot = "counts", assay = "RNA",pt.size = 0, combine = FALSE)
+p5 = DotPlot(objs, features = rev(all_markers.to.plot), cols = c("blue", "red"), dot.scale = 8, ) + RotatedAxis()
 
 
 
-VlnPlot(objs, features = c("CD8A"))
 
-VlnPlot(objs, features = c("CD4"))
+# Create Seurat Object of T-Cell Clusters
+DefaultAssay(objs) <- "RNA"
+Tcells <- subset(x = objs, subset =CD3D > 0 | CD3E > 0 | CD3G > 0)
+#Tcells = subset(objs, cells = WhichCells(objs, idents= c("0", "5", "2", "11", "13", "6")) )
+tcell_list2 <- SplitObject(Tcells, split.by = "label")
+tcell_list2 <- lapply(tcell_list2, SCTransform, vars.to.regress = "percent.mt")
 
+tcell_combined2 <-SCT_intergration(tcell_list2)
 
-plots <- VlnPlot(objs, features = c("CD4", "CD8A"), split.by = "label", slot = "counts",
-    pt.size = 0, combine = FALSE)
+pt1 <- DimPlot(tcell_combined2, group.by = "orig.ident")
+pt2 <- DimPlot(tcell_combined2, label = TRUE, repel = TRUE)
+pt3 <- DimPlot(tcell_combined2, label=TRUE, reduction = "umap", split.by = "label")
+pt4 <- DoHeatmap(tcell_combined2, features = tcell_markers.to.plot , assay = "RNA", slot = "data", angle = 90) + scale_fill_gradientn(colors = c("white", "red"))
+plotst <- VlnPlot(tcell_combined2, features = c("CD4", "CD8A","CD8B"), split.by = "label", slot = "counts", assay = "RNA",pt.size = 0, combine = FALSE)
+pt5 = DotPlot(tcell_combined2, features = rev(all_markers.to.plot), cols = c("blue", "red"), dot.scale = 8, ) + RotatedAxis()
 
-markers.to.plot <- c("CD4","CD8A","CD3D", "CREM", "HSPH1", "SELL", "GIMAP5", "CACYBP", "GNLY", "NKG7", "CCL5", 
-     "MS4A1", "CD79A", "MIR155HG", "NME1", "FCGR3A", "VMO1", "CCL2", "S100A9", "HLA-DQA1", 
-    "GPR183", "PPBP", "GNG11", "HBA2", "HBB", "TSPAN13", "IL3RA", "IGJ")
-p5 = DotPlot(objs, features = rev(markers.to.plot), cols = c("blue", "red"), dot.scale = 8, ) + RotatedAxis()
 
 outpdf=paste("UMAP","_all.pdf",sep='')
 pdf(outpdf, width = 16, height = 10)
 
 #print(ggarrange(p1,p2, ncol = 2, nrow = 1,widths=c(1, 1)))
 print(p1+p2)
-print(p3)
+grid::grid.newpage()
+print(p33)
 print(p4)
-CombinePlots(plots = plots, ncol = 1)
+CombinePlots(plots = plots0, ncol = 1)
+print(p3)
 print(p5)
+
+print(pt1+pt2)
+print(pt4)
+CombinePlots(plots = plotst, ncol = 1)
+print(pt3)
+print(pt5)
 dev.off()
 
 
-#### Identify   cell type  
+#### Identify  T cell type  
 
+Tcells = subset(objs, cells = WhichCells(objs, idents= c("0", "5", "2", "11", "13", "6")) )
 
 
 objs<- PrepSCTFindMarkers(objs,assay = "SCT", verbose = TRUE)
@@ -97,8 +158,6 @@ DEG <- FindAllMarkers(objs,
 
 mark_gene <- DEG %>% mutate(avg_logFC=avg_log2FC) %>% filter(p_val_adj<0.05)
 
-signature <- readxl::read_excel('Cell_marker_Human.xlsx')
 
-sig_gene  <- signature %>% as.data.frame() %>% filter(`Tissue type`=="Peripheral blood" & `Cell type`== "Cancer cell") %>% mutate(V1=`Cell name`,V2=Symbol) %>% select(V1,V2)  %>% unique(.) 
 
 
