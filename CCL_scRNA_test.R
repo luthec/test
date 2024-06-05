8fa8e46663dfd19ff0c8a499a2cabae517169bc1
luthec <tiancliu@gmail.com>
2024-06-05 17:12:06 +0800

    1

--- CCL_scRNA_test.R
+++ CCL_scRNA_test.R
@@ -1,69 +1,78 @@
 library(dplyr)
 library(Seurat)
-library(metap)
 library(ggrepel)
 library(ggpubr)
+
+
+SCT_intergration <- function(assay_list,n_dim=20) {  
+features <- SelectIntegrationFeatures(object.list = assay_list, nfeatures = 3000)
+datasets <- PrepSCTIntegration(object.list = assay_list, anchor.features = features, verbose = TRUE)
+anchors <- FindIntegrationAnchors(object.list = datasets, normalization.method = "SCT",anchor.features = features, verbose = TRUE, reference=1,reduction = "cca")
+IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE, k.weight = 46)
+}
+
+Clustering_Cells <- function(x,n_dim=20) {
+# x <- NormalizeData(x)
+# x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
+x <- ScaleData(x, verbose = FALSE)
+x <- RunPCA(x, npcs = 30, verbose = FALSE)
+x <- RunUMAP(x, reduction = "pca", dims = 1:n_dim)
+x <- FindNeighbors(x, reduction = "pca", dims = 1:n_dim)
+FindClusters(x, resolution = 0.5, algorithm = 2)
+}
+
+##########start
+
 
 folders=list.files('./')
 folders
 
 sceList = lapply(folders,function(folder){ 
-  CreateSeuratObject(counts = Read10X(paste0(folder,"/filtered_feature_bc_matrix")),  project = folder)
+  CreateSeuratObject(counts = Read10X(folder),  project = folder)
 })
 
 for(i in 1:length(folders)){
-  sceList[[i]][["percent.mt"]] <- PercentageFeatureSet(sceList[[i]], pattern = "^MT-")
+  print(i)
+  sceList[[i]] <- PercentageFeatureSet(sceList[[i]], pattern = "^MT-", col.name = "percent.mt") 
+  sceList[[i]] <- subset(sceList[[i]],subset = nCount_RNA >200 & percent.mt < 20) %>% 
+  SCTransform(vars.to.regress = "percent.mt", verbose = FALSE,vst.flavor = "v2")
 }
 
 names(sceList)  = folders
 sum(sapply(sceList, function(x)ncol(x@assays$RNA@counts)))
 
+for(i in 1:10) { sceList[[i]] $label <-"CLL"}
+for(i in 11:17) { sceList[[i]] $label <-"NC"}
+
+objs <-SCT_intergration(sceList[1:17])
+objs.tcells <- subset(x = objs, subset =CD3D > 0 | CD3E > 0 | CD3G > 0)
+
+tcells.cluster = Clustering_Cells(objs.tcells)
+
 # Set up control object
-nc <- merge(x = sceList[[8]], y = sceList[[9]], add.cell.ids = names(sceList)[8:9] , project = "NC")
+
+nc <-SCT_intergration(sceList[11:17])
 nc$label <- "NC"
-nc <- NormalizeData(nc, verbose = FALSE)
-nc <- FindVariableFeatures(nc, selection.method = "vst", nfeatures = 2000)
 
-# Set up case object
-ccl <- merge(x = sceList[[1]], y = sceList[2:6], add.cell.ids = names(sceList)[1:6] , project = "CCL")
-ccl$label <- "CCL"
-ccl <- NormalizeData(ccl, verbose = FALSE)
-ccl <- FindVariableFeatures(ccl, selection.method = "vst", nfeatures = 2000)
+# DefaultAssay(nc) <- 'RNA'
+# nc <- JoinLayers(nc)
 
-#jsh3
-jsh <- sceList[[7]]
-jsh$label <- "JSH"
-jsh<- NormalizeData(jsh, verbose = FALSE)
-jsh <- FindVariableFeatures(jsh, selection.method = "vst", nfeatures = 2000)
-
-###normalize, integration and filter
-ccl.anchors <- FindIntegrationAnchors(object.list = list(nc, ccl, jsh), dims = 1:20)
-ccl.combined <- IntegrateData(anchorset = ccl.anchors, dims = 1:20)
-DefaultAssay(ccl.combined) <- "integrated"
-
-#######UMAP
-
-ccl.combined <- ScaleData(ccl.combined, verbose = FALSE)
-ccl.combined <- RunPCA(ccl.combined, npcs = 30, verbose = FALSE)
-ccl.combined <- RunUMAP(ccl.combined, reduction = "pca", dims = 1:30)
-ccl.combined <- FindNeighbors(ccl.combined, reduction = "pca", dims = 1:30)
-ccl.combined <- FindClusters(ccl.combined, resolution = 0.5)
-
-p1 <- DimPlot(ccl.combined, reduction = "umap", group.by = "orig.ident")
-p2 <- DimPlot(ccl.combined, reduction = "umap", label = TRUE, repel = TRUE)
-
-p3 <- DimPlot(ccl.combined, reduction = "umap", split.by = "label")
-p4 <- FeaturePlot(ccl.combined, features = c("CD8A", "IL7R", "CCR7", "S100A4", "GNLY", "NKG7", "FCGR3A", "MS4A7", "CD14"), min.cutoff = "q9")
-
-VlnPlot(ccl.combined, features = c("CD8A"))
-
-VlnPlot(ccl.combined, features = c("IL7R"))
+cll <-SCT_intergration(sceList[1:10])
+cll$label <- "CLL"
 
 
-plots <- VlnPlot(ccl.combined, features = c("IL7R", "CCR7", "S100A4"), split.by = "label", slot = "counts",
-    pt.size = 0, combine = FALSE)
-CombinePlots(plots = plots, ncol = 1)
+DefaultAssay(cll) <- 'SCT'
+DefaultAssay(nc) <- 'SCT'
 
+nc.bcells <- subset(x = nc, subset =MS4A1 > 0 | CD79A > 0 | CD19 > 0)
+cll.bcells <- subset(x = cll, subset =MS4A1 > 0 | CD79A > 0 | CD19 > 0)
+
+#######figure
+p1 <- DimPlot(tcells.cluster, reduction = "umap", group.by = "orig.ident")
+p2 <- DimPlot(tcells.cluster, reduction = "umap", label = TRUE, repel = TRUE)
+
+p3 <- DimPlot(tcells.cluster, reduction = "umap", split.by = "label")
+p4 <- FeaturePlot(tcells.cluster, features = c("CD8A", "GZMK", "CCL5", "CCR7"), min.cutoff = "q9")
 
 outpdf=paste("UMAP","_all.pdf",sep='')
 pdf(outpdf, width = 16, height = 10)
@@ -73,42 +82,10 @@ print(p1+p2)
 print(p3)
 print(p4)
 
+VlnPlot(tcells.cluster, features = c("CD8A"))
+VlnPlot(tcells.cluster, features = c("ITGA4"))
+VlnPlot(tcells.cluster, features = c("BCL2"))
 
 dev.off()
 
-#### Identify   cell type  
 
-ccl.aggregated <- sumCountsAcrossCells(ccl.combined,  BPPARAM=bpp)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#### Identify conserved cell type markers
-
-DefaultAssay(ccl.combined) <- "RNA"
-con.markers <- FindConservedMarkers(ccl.combined, ident.1 = 0, grouping.var = "label", verbose = FALSE)
-head(con.markers)
-
-FeaturePlot(ccl.combined, features = c("CD3D", "SELL", "CREM", "CD8A", "GNLY", "CD79A", "FCGR3A", 
-    "CCL2", "PPBP"), min.cutoff = "q9")
-
-
-ccl.markers <- FindAllMarkers(ccl.combined, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-ccl.markers %>%
-    group_by(cluster) %>%
-    slice_max(n = 2, order_by = avg_log2FC)
-
-IL7R, CCR7
-
-VlnPlot(ccl.combined, features = c("IL7R", "CCR7"))
