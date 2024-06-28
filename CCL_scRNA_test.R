7c8c60c31aa9bec58b61d2bddc862afe37d56617
luthec <tiancliu@gmail.com>
2024-06-28 19:18:46 +0800

    1

--- CCL_scRNA_test.R
+++ CCL_scRNA_test.R
@@ -14,6 +14,7 @@ IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE,
 Clustering_Cells <- function(x,n_dim=20) {
 # x <- NormalizeData(x)
 # x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
+DefaultAssay(x) <- "integrated"
 x <- ScaleData(x, verbose = FALSE)
 x <- RunPCA(x, npcs = 30, verbose = FALSE)
 x <- RunUMAP(x, reduction = "pca", dims = 1:n_dim)
@@ -39,15 +40,55 @@ for(i in 1:length(folders)){
 }
 
 names(sceList)  = folders
-sum(sapply(sceList, function(x)ncol(x@assays$RNA@counts)))
+
+##sum(sapply(sceList, function(x)ncol(x@assays$RNA@counts)))
 
 for(i in 1:10) { sceList[[i]] $label <-"CLL"}
 for(i in 11:17) { sceList[[i]] $label <-"NC"}
 
 objs <-SCT_intergration(sceList[1:17])
-objs.tcells <- subset(x = objs, subset =CD3D > 0 | CD3E > 0 | CD3G > 0)
 
+DefaultAssay(objs) <- 'SCT'
+
+objs.tcells <- subset(x = objs, subset =CD3D > 0 | CD3E > 0 | CD3G > 0)
 tcells.cluster = Clustering_Cells(objs.tcells)
+
+objs.bcells <- subset(x = objs, subset =MS4A1 > 0 | CD79A > 0 | CD19 > 0)
+bcells.cluster = Clustering_Cells(objs.bcells)
+
+
+#######figure
+p1 <- DimPlot(tcells.cluster, reduction = "umap", group.by = "orig.ident")
+p2 <- DimPlot(tcells.cluster, reduction = "umap", label = TRUE, repel = TRUE)
+
+p3 <- DimPlot(tcells.cluster, reduction = "umap", split.by = "label", label = TRUE)
+p4 <- FeaturePlot(tcells.cluster, features = c("CD8A", "GZMK", "CCL5", "CCR7","ITGA4","BCL2"), min.cutoff = "q9")
+
+
+p5 <- DimPlot(bcells.cluster, reduction = "umap", group.by = "orig.ident")
+p6 <- DimPlot(bcells.cluster, reduction = "umap", label = TRUE, repel = TRUE)
+p7 <- DimPlot(bcells.cluster, reduction = "umap", split.by = "label", label = TRUE)
+
+outpdf=paste("UMAP","_all.pdf",sep='')
+pdf(outpdf, width = 16, height = 10)
+
+print(p1+p2)
+print(p3)
+print(p4)
+
+VlnPlot(tcells.cluster, features = c("CD8A"))
+VlnPlot(tcells.cluster, features = c("ITGA4"))
+VlnPlot(tcells.cluster, features = c("BCL2"))
+
+print(p5+p6)
+print(p7)
+
+dev.off()
+
+
+
+
+
 
 # Set up control object
 
@@ -66,26 +107,5 @@ DefaultAssay(nc) <- 'SCT'
 
 nc.bcells <- subset(x = nc, subset =MS4A1 > 0 | CD79A > 0 | CD19 > 0)
 cll.bcells <- subset(x = cll, subset =MS4A1 > 0 | CD79A > 0 | CD19 > 0)
-
-#######figure
-p1 <- DimPlot(tcells.cluster, reduction = "umap", group.by = "orig.ident")
-p2 <- DimPlot(tcells.cluster, reduction = "umap", label = TRUE, repel = TRUE)
-
-p3 <- DimPlot(tcells.cluster, reduction = "umap", split.by = "label", label = TRUE)
-p4 <- FeaturePlot(tcells.cluster, features = c("CD8A", "GZMK", "CCL5", "CCR7","ITGA4","BCL2"), min.cutoff = "q9")
-
-outpdf=paste("UMAP","_all.pdf",sep='')
-pdf(outpdf, width = 16, height = 10)
-
-#print(ggarrange(p1,p2, ncol = 2, nrow = 1,widths=c(1, 1)))
-print(p1+p2)
-print(p3)
-print(p4)
-
-VlnPlot(tcells.cluster, features = c("CD8A"))
-VlnPlot(tcells.cluster, features = c("ITGA4"))
-VlnPlot(tcells.cluster, features = c("BCL2"))
-
-dev.off()
 
 
