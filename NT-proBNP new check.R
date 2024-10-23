e6bc67084f43004fb071f91e4e637eb0d98dd47d
luthec <tiancliu@gmail.com>
2024-10-23 15:26:59 +0800

    1

--- NT-proBNP new check.R
+++ NT-proBNP new check.R
@@ -2,6 +2,46 @@ library(dplyr)
 library(xlsx)
 library(readxl)
 
+###basic check 01
+
+dt = read_excel(dir()[1],sheet = 2,col_names = T)
+
+table(dt$组别)
+
+dt %>% filter(!grepl("^S{1}\\d{6}$", 筛选号))
+
+dtt = dt %>% filter(!grepl("^P{1}\\d{3}$", 样本编号))
+table(dtt$样本编号)
+
+table(dt$性别)
+
+sapply(dt$`年龄（岁）`, is.numeric)
+
+dtt = dt %>% filter(!grepl("^\\d+", `NT-proBNP 检测结果（ng/L）`))
+table(dtt$`NT-proBNP 检测结果（ng/L）`)
+
+############ 02
+dt = read_excel(dir()[1],sheet = 4,col_names = T)
+
+table(dt$组别)
+
+dt %>% filter(!grepl("^S{1}\\d{5}$", 筛选号))
+
+dtt = dt %>% filter(!grepl("^R{1}\\d{5}$", 样本编号))
+table(dtt$样本编号)
+
+table(dt$性别)
+
+sapply(dt$`年龄（岁）`, is.numeric)
+
+dtt = dt %>% filter(!grepl("^\\d+", `NT-proBNP 检测结果（ng/L）`))
+table(dtt$`NT-proBNP 检测结果（ng/L）`)
+
+
+
+
+#####new
+
 ifNA <- function(x){is.na(x) | x=="NA"}
 
 query <- function(mat){
