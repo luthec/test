e02e1592c0a739c50587a73fe4a282934fd60538
luthec <tiancliu@gmail.com>
2024-12-18 11:28:32 +0800

    1

--- align.R
+++ align.R
@@ -17,3 +17,21 @@ print(inter_samples)
 }
 
 dir()[1]  %>% two_a()
+
+############
+
+two_a2 <- function(file1,file2){
+
+mat1 = read_excel(file1,skip=4,sheet = 1,col_names = T)
+mat2 = read_excel(file2,skip=4,sheet = 1,col_names = T)
+
+inter_samples=intersect(mat1$筛选号,mat2$筛选号)
+create_output_table(compare_df(arrange(mat1 %>% filter(筛选号 %in% inter_samples) , 筛选号),arrange(mat2 %>% filter(筛选号 %in% inter_samples) , 筛选号)), output_type = 'xlsx', file_name = paste0(file,"_queries.xlsx"))
+
+print(inter_samples)
+
+}
+
+two_a2("CHN-231_CRF_CRC_YSQ_20241212Sheet1.xlsx","CHN-231_CRF_CRC_ZXN20241212_Sheet1  .xlsx")
+
+two_a2("CHN-231_CRF_CRC_YSQ_20241212Sheet2.xlsx","CHN-231_CRF_CRC_ZXN_20241212_Sheet2.xlsx")
--- data_check_BT.R
+++ data_check_BT.R
@@ -1,27 +1,43 @@
+library(arsenal)
+library(compareDF)
+library(dplyr)
+library(xlsx)
+library(readxl)
+library(readr)
+
+
 #######Ferritin
 
+no_value=c("No Value","Cancelled","No result",NA)
+
+bt = read_excel("Gannan_BT.xlsx") 
+colnames(bt) = c("Sample_ID","Test_ID","Predicate_ID")
+
 ins_markers=c("Inhibin A_IUO")
-ins = read_csv(dir()[2])%>% 
-            rename(Test_ID="SampleID")
+ins = read_csv("gannan Dxl9000Results_CSV_100%.csv")%>% 
+            filter(! `DoseResult` %in% no_value) %>%
+            filter(! `DoseUnit`=="RLU") %>%
+            rename(Test_ID="SampleID") %>% 
+            mutate(Test_ID = gsub("R[1-9]","",Test_ID))
+            
+            
 
 predicate_markers=c("InhibinA")
-predicate = read_csv(dir()[1],quote = "") %>% 
-            rename(Predicate_ID="Sample ID")
+predicate = read_csv("gannan A2Result_CSV_100%.csv",quote = "") %>%  
+            filter(! `Result` %in% no_value) %>%
+            rename(Predicate_ID = "Sample ID") %>%
+#            mutate(Predicate_ID = gsub("R[1-9]","",Predicate_ID))
 
-bt = read_excel(dir()[3]) 
-colnames(bt) = c("Sample_ID","Test_ID","Predicate_ID")
 
 mat1 = bt %>% 
        left_join(ins  %>% filter(`TestName` %in% ins_markers),by="Test_ID") %>% 
-       left_join(predicate  %>% filter(`Test Name` %in% predicate_markers) 
-                            %>% filter(`Test Name` %in% predicate_markers)
-       ,by="Predicate_ID") %>% 
+       left_join(predicate  %>% filter(`Test Name` %in% predicate_markers) ,by="Predicate_ID") %>% 
        select(Sample_ID,DoseResult,Result)
 
 # write_excel_csv(ins %>% filter(TestName %in% markers) %>% select(SampleID,TestName,DoseResult),  paste0(markers[1],"_wrongnumbers.csv"))
 
 
-mat2 = read_excel(dir()[4], sheet = 1, skip = 3,col_types ="text") %>% 
+mat2 = read_excel("CHN-216_A_DSF-.xlsx", sheet = 2, skip = 3,col_types ="text") %>% 
        rename_at(7,~"DoseResult") %>% 
        rename_at(8,~"Result") %>% 
        rename(Sample_ID="唯一可溯源编号") %>% 
@@ -30,4 +46,6 @@ mat2 = read_excel(dir()[4], sheet = 1, skip = 3,col_types ="text") %>%
 
 inter_samples=intersect(mat1$Sample_ID,mat2$Sample_ID)
 
-create_output_table(compare_df(arrange(mat1 %>% filter(Sample_ID %in% inter_samples) , Sample_ID),arrange(mat2 %>% filter(Sample_ID %in% inter_samples) , Sample_ID)), output_type = 'xlsx', file_name = paste0(markers[1],"_queries.xlsx"))
+reform_mat2 =rbind(mat2 %>% distinct(), mat2[duplicated(mat2$Sample_ID),])
+
+create_output_table(compare_df(arrange(mat1 %>% filter(Sample_ID %in% inter_samples) , Sample_ID), reform_mat2 ), output_type = 'xlsx', file_name = paste0(ins_markers[1],"_queries.xlsx"))
