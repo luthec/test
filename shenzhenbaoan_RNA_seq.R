3659d5a45d36a683229a4a9d55527c0ddec86c11
luthec <tiancliu@gmail.com>
2025-07-31 16:08:15 +0800

    1

--- shenzhenbaoan_RNA_seq.R
+++ shenzhenbaoan_RNA_seq.R
@@ -46,12 +46,54 @@ objs <- IntegrateLayers(
 objs <- FindNeighbors(objs, reduction = "harmony", dims = 1:30)
 objs <- FindClusters(objs, resolution = 2, cluster.name = "harmony_clusters")
 objs <- RunUMAP(objs, reduction = "harmony", dims = 1:30, reduction.name = "umap.harmony")
+objs <- JoinLayers(objs)
 
 ###cell type annotation
+options(timeout=1000)
 objs <- RunAzimuth(objs, reference = "pbmcref")
 
+###DE
+
+cell_type = objs$predicted.celltype.l1 %>% table() %>% names()
+objs$celltype.condition <- paste(objs$predicted.celltype.l1, objs$label, sep = "_")
+Idents(objs) <- "celltype.condition"
+
+##MAST
+for (i in 1:length(cell_type)){
+  print(cell_type[i])
+  FindMarkers(objs, ident.1 = paste0(cell_type[i],"_TB"), ident.2 = paste0(cell_type[i],"_HC"), verbose = FALSE,test.use = "MAST") %>% 
+  rownames_to_column(var = "gene_names_col") %>%
+  filter(gene_names_col %in% c("TNFAIP8","TNFAIP8L1","TNFAIP8L2","TNFAIP8L3"))
+}
+
+##
+# pseudobulk the counts based on donor-condition-celltype
+pseudo_objs <- AggregateExpression(objs, assays = "RNA", return.seurat = T, group.by = c("label", "orig.ident", "predicted.celltype.l1"))
+head(Cells(pseudo_objs))
+
+pseudo_objs$celltype.condition <- paste(pseudo_objs$predicted.celltype.l1, pseudo_objs$label, sep = "_")
+Idents(pseudo_objs) <- "celltype.condition"
+
+print("TB vs LTBI")
+for (i in 1:length(cell_type)){
+  print(cell_type[i])
+  FindMarkers(pseudo_objs, ident.1 = paste0(cell_type[i],"_TB"), ident.2 = paste0(cell_type[i],"_LTBI"), min.cells.group = 2, verbose = FALSE, test.use = "DESeq2") %>% 
+  rownames_to_column(var = "gene_names_col") %>%
+  filter(gene_names_col %in% c("TNFAIP8","TNFAIP8L1","TNFAIP8L2","TNFAIP8L3")) %>% print()
+}
+
+print("TB vs HC")
+for (i in 1:length(cell_type)){
+  print(cell_type[i])
+  FindMarkers(pseudo_objs, ident.1 = paste0(cell_type[i],"_TB"), ident.2 = paste0(cell_type[i],"_HC"), min.cells.group = 2, verbose = FALSE, test.use = "DESeq2") %>% 
+  rownames_to_column(var = "gene_names_col") %>%
+  filter(gene_names_col %in% c("TNFAIP8","TNFAIP8L1","TNFAIP8L2","TNFAIP8L3")) %>% print()
+}
+
+write.table(pseudo_objs@active.ident, file='Convert_UMI_Label.tsv', quote=FALSE, sep='\t', col.names = TRUE)
+write.table(pseudo_objs@assays[["RNA"]]$counts, file='Gene_Count_per_Cell.tsv', quote=FALSE, sep='\t', col.names = TRUE)
 ###figure
-objs <- JoinLayers(objs)
+
 
 p1 <- DimPlot(objs,group.by = "predicted.celltype.l1", split.by = "label", label = TRUE)
 p2 <- FeaturePlot(objs,split.by = "label",features = c('TNFAIP8','TNFAIP8L1'), min.cutoff = "q9")
